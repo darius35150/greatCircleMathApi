3be975e14d88b608829dd7aab149f7e256156d20
darius35150 <darius35150@yahoo.com>
2026-02-27 17:34:21 -0600

    Removed unnecessary endpoint and change from path variables to request params.

--- src/main/java/com/holleysoft/greatCircleMathApi/controller/GreatCircleMathCalculationController.java
+++ src/main/java/com/holleysoft/greatCircleMathApi/controller/GreatCircleMathCalculationController.java
@@ -2,11 +2,12 @@ package com.holleysoft.greatCircleMathApi.controller;
 
 import com.holleysoft.greatCircleMathApi.model.GreatCircleMathCalculationDto;
 import com.holleysoft.greatCircleMathApi.service.GreatCircleMathCalculationService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/calculate")
@@ -17,29 +18,17 @@ public class GreatCircleMathCalculationController {
         this.greatCircleMathCalculationService = greatCircleMathCalculationService;
     }
 
-    @GetMapping("/getDistance/{latA}/{longA}/{latB}/{longB}")
+    @GetMapping("/getDistance")
     public ResponseEntity<GreatCircleMathCalculationDto> getGreatCircleMathCalculation(
-            @PathVariable double latA,
-            @PathVariable double longA,
-            @PathVariable double latB,
-            @PathVariable double longB){
+            @RequestParam(required = true) double latA,
+            @RequestParam(required = true) double longA,
+            @RequestParam(required = true) double latB,
+            @RequestParam(required = true) double longB,
+            @RequestParam(required = false, defaultValue = "mi") String unit){
 
         ResponseEntity<GreatCircleMathCalculationDto> responseEntity;
-        responseEntity = greatCircleMathCalculationService.calculateGreatCircleMath(latA, longA, latB, longB);
 
-        return responseEntity;
-    }
-
-    @GetMapping("/getDistanceInKm/{latA}/{longA}/{latB}/{longB}")
-    public ResponseEntity<GreatCircleMathCalculationDto> getGreatCircleMathCalculationInKM(
-            @PathVariable double latA,
-            @PathVariable double longA,
-            @PathVariable double latB,
-            @PathVariable double longB){
-
-        ResponseEntity<GreatCircleMathCalculationDto> responseEntity;
-        responseEntity = greatCircleMathCalculationService.calculateGreatCircleMath(latA, longA, latB, longB, true);
-
+        responseEntity = greatCircleMathCalculationService.calculateGreatCircleMath(latA, longA, latB, longB, unit.equalsIgnoreCase("mi") ? false : true);
         return responseEntity;
     }
 }
